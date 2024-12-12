4fe38131ad002a79de1feb6884133981ef4cfbe9
Cornilious Nolasco <115691686+cornnahum@users.noreply.github.com>
2024-12-12 14:18:29 +0100

    adding footer

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import NavbarComponent from './components/Navbar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+import Footer from './components/Footer';
+import './styles/components/footer.css';
 
 function App() {
   return (
     <Router>
       <NavbarComponent />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
+      <Footer />
     </Router>
   );
 }
